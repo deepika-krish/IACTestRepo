8519e2cd6d530b7ac4d893b709b5bed75c334ee5
Deepika <deepika.krishnamoorthy@translink.ca>
2025-10-01 13:07:12 -0700

    Fix subtraction calculation in Main method

--- Program.cs
+++ Program.cs
@@ -11,8 +11,8 @@ namespace IACTestRepo
             Console.WriteLine($"Direct calculation: 11.5 + 6.3 = {addResult}");
 
             ConsoleSub calculatorSub = new ConsoleSub();
-            double subResult = calculatorSub.Subtract(10.5, 5.3);
-            Console.WriteLine($"Direct calculation: 10.5 - 5.3 = {subResult}");
+            double subResult = calculatorSub.Subtract(11.5, 6.3);
+            Console.WriteLine($"Direct calculation: 11.5 - 6.3 = {subResult}");
 
             ConsoleMul calculatorMul = new ConsoleMul();
             double mulResult = calculatorMul.Multiply(10.5, 5.3);

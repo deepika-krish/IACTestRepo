1afe1e24d996668742ee437df5dcb17e6cf9c189
Deepika <deepika.krishnamoorthy@translink.ca>
2025-10-01 11:49:55 -0700

    Add training message to Main method

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace IACTestRepo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World - TEST!");
+            Console.WriteLine("Training Day 1");
         }
     }
 }

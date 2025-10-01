7b5977ca4a1e10d62a15b2b70b03483631c696db
Deepika <deepika.krishnamoorthy@translink.ca>
2025-10-01 11:36:37 -0700

    Update greeting message in Main method to include '- TEST!'

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace IACTestRepo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World - TEST!");
         }
     }
 }
